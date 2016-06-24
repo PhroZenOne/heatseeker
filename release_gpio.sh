240bbc749d2034207b64a60ae74fb3763d641f9f
anton.stenmark <anton.stenmark@arrowheadgs.com>
2016-06-25 00:24:06 +0200

    Added script to release GPIO ports if the program crashes.

---
+++ release_gpio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 17 > /sys/class/gpio/unexport
+echo 27 > /sys/class/gpio/unexport
+echo 22 > /sys/class/gpio/unexport
+echo 10 > /sys/class/gpio/unexport
+echo  9 > /sys/class/gpio/unexport
+echo 11 > /sys/class/gpio/unexport
